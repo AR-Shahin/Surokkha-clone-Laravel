cca0d84bd6640007770ac02886030df39d20ba7b
AR-Shahin <mdshahinmije96@gmail.com>
2022-04-10 11:30:14 +0600

    issue fix

--- resources/views/components/utility/captca-html.blade.php
+++ resources/views/components/utility/captca-html.blade.php
@@ -3,7 +3,10 @@
         <div class="col-md-5">
             <div class="card">
                 <div class="card-body">
-                    <p class="captca text-center text-success" style="font-weight:bold"></p>
+                    <em>
+                        <p class="captca text-center text-success"
+                            style="font-weight:bold;font-family:'Courier New', Courier, monospace"></p>
+                    </em>
                     <p class="text-center">উপরের লিখাটি সঠিকভাবে নিচে লিখুন</p>
                     <input type="text" class="form-control captca_input" placeholder="উপরের লিখাটি সঠিকভাবে নিচে লিখুন">
                 </div>
