16172df83e402f6a1f768c4db33a568bc7e9dcc3
AR-Shahin <mdshahinmije96@gmail.com>
2022-04-10 11:53:04 +0600

    admin dynamic dasgboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Admin;
+use App\Models\Doctor;
+use App\Models\Hospital;
+use App\Models\User;
+use App\Models\Vaccine;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('Backend.dashboard');
+        return view('Backend.dashboard', [
+            'vaccine' => Vaccine::count(),
+            'user' => User::count(),
+            'hospital' => Hospital::count(),
+            'doctor' => Doctor::count(),
+        ]);
     }
 }
