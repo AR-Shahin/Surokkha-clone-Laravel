148dd362f4a1120109c176218b61c5c943b3138e
AR-Shahin <mdshahinmije96@gmail.com>
2022-04-10 11:55:08 +0600

    add website link in admin panel

--- resources/views/Backend/includes/sidebar.blade.php
+++ resources/views/Backend/includes/sidebar.blade.php
@@ -22,57 +22,13 @@
         <!-- Sidebar Menu -->
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-
-                {{-- <li class="nav-item menu-open">
-            <a href="#" class="nav-link ">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-                Dashboard
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="./index.html" class="nav-link active">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v1</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="./index2.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v2</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="./index3.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v3</p>
-                </a>
-              </li>
-            </ul>
-          </li> --}}
-                {{-- <li class="nav-item">
-            <a href="@route('category.index')" class="nav-link">
-              <i class="nav-icon far fa-circle text-warning"></i>
-              <p>Category</p>
-            </a>
-          </li>
-
-        </li>
-        <li class="nav-item">
-            <a href="@route('cache')" class="nav-link">
-              <i class="nav-icon far fa-circle text-warning"></i>
-              <p>Cache</p>
-            </a>
-          </li>
-        <li class="nav-item">
-          <a href="@route('skill.index')" class="nav-link">
-            <i class="nav-icon far fa-circle text-warning"></i>
-            <p>Skill</p>
-          </a>
-        </li> --}}
                 @auth('admin')
+                    <li class="nav-item">
+                        <a target="_blank" class="nav-link" href="/">
+                            <i class="nav-icon fa fa-globe text-warning"></i>
+                            <p>Website</p>
+                        </a>
+                    </li>
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('admin.dashboard') }}">
                             <i class="nav-icon fa fa-home text-warning"></i>
